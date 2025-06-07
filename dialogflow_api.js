e24af775e7ec4af3bc536af7c1a6172a2abba59a
jamrats117 <86391050+jamrats117@users.noreply.github.com>
2025-06-07 20:43:29 +0700

    Create dialogflow_api.js

---
+++ dialogflow_api.js
@@ -0,0 +1,50 @@
+var json = {
+              "responseId": "65865fc1-1f08-44f4-b177-231606c79fe6-996f169b",
+              "queryResult": {
+                "queryText": "ใช่",
+                "action": "get_INR.get_INR-yes",
+                "parameters": {},
+                "allRequiredParamsPresent": true,
+                "fulfillmentMessages": [
+                  {
+                    "text": {
+                      "text": [
+                        ""
+                      ]
+                    }
+                  }
+                ],
+                "outputContexts": [
+                  {
+                    "name": "projects/herb-v2-9li9/agent/sessions/28b0d05f-941e-bebb-fa58-0ac0569d815e/contexts/get_inr-followup",
+                    "lifespanCount": 1,
+                    "parameters": {
+                      "herb.original": "กระเทียม",
+                      "herb": "กระเทียม"
+                    }
+                  }
+                ],
+                "intent": {
+                  "name": "projects/herb-v2-9li9/agent/intents/84619339-a55d-486f-a309-718e3ee43fc3",
+                  "displayName": "get_INR - yes"
+                },
+                "intentDetectionConfidence": 1,
+                "languageCode": "th"
+              }
+            };
+
+
+
+var json2 = {
+              "responseId": "...",
+              "queryResult": {
+                "queryText": "ใช่",
+                "action": "get_INR.get_INR-yes",
+                "parameters": {},
+                "fulfillmentMessages": [],
+                "outputContexts": [],
+                "intent": {},
+                "languageCode": "th"
+              }
+            };
+
